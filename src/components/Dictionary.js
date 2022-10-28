8f76241978442f8d0e403861a5582fc761436e98
p2lifau <p2lifau@gmail.com>
2022-10-28 12:39:09 -0700

    added dictionary

---
+++ src/components/Dictionary.js
@@ -0,0 +1,41 @@
+import './Dictionary.css'
+
+export default function Dictionary() {
+  return (
+    <div className="dictionary-cards">
+        <div className='card-1'>
+        <img src="../../img/samoa-map.png" alt="" />
+        <h2>Map</h2>
+        <p>Western Samoa</p>
+        </div>
+         <div className="card-2">
+         <img src="../../img/taupou.jpg" alt="" />
+         <h2>Taupou</h2>
+         <p>Female dancer</p>
+        </div>
+        <div className="card-3">
+        <img src="../../img/falesand-2.jpg" alt="" />
+        <h2>Fale Samoa</h2>
+        <p>Traditional Samoan house</p>
+        </div>
+        <div className="card-4">
+        <img src="../../img/sandula.jpg" alt="" />
+        <h2>Tanoa</h2>
+        <p>Ava Bowl used for traditional community ceremony</p>
+        </div>
+        <div className="card-5">
+        <img src="../../img/siva-afi.jpg" alt="" />
+        <h2>Siva Afi</h2>
+        <p>Fire knife dancer</p>
+        </div>
+        <div className="card-6">
+        <img src="../../img/manaia.jpg" alt="" />
+        <h2>Manaia</h2>
+        <p>Male dancer</p>
+        </div>
+        
+
+    </div>
+    
+  )
+}
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,27 +1,22 @@
-import { useState, useEffect} from 'react';
-import './App.css';
-import Dictionary from './components/Dictionary';
-import Instructions from './components/Instructions';
-import SoloCard from './components/SoloCard';
+import React from 'react'
+import { useState , useEffect} from 'react'
 
-// 6 different cards with src to picture
+
 const cardPics = [
-  {"src": "/img/falesand-2.jpg" , matched: false},
-  {"src": "/img/taupou.jpg" , matched: false},
-  {"src": "/img/manaia.jpg" , matched: false},
-  {"src": "/img/sandula.jpg" , matched: false},
-  {"src": "/img/siva-afi.jpg" , matched: false},
-  {"src": "/img/samoa-map.png" , matched: false}
-]
-function App() {
+    {"src": "/img/falesand-2.jpg" , matched: false},
+    {"src": "/img/taupou.jpg" , matched: false},
+    {"src": "/img/manaia.jpg" , matched: false},
+    {"src": "/img/sandula.jpg" , matched: false},
+    {"src": "/img/siva-afi.jpg" , matched: false},
+    {"src": "/img/samoa-map.png" , matched: false}
+  ]
+
   const [cards, setCards] = useState([])
   // how many trys a user takes to complete the game
   const [trys, setTrys] = useState(0)
   const [choiceUno, setChoiceUno] = useState(null)
   const [choiceDos, setChoiceDos] = useState(null)
   const [disabled, setDisabled] = useState(false)
-
-  // shuffle cards
   const shuffleCards = () => {
     // duplicate the arrays using spread operator, total of 12 (2 of each pics)
     const shuffledCards = [...cardPics, ...cardPics]
@@ -35,9 +30,8 @@ function App() {
         setCards(shuffledCards)
         setTrys(0)
   }
-
-  // compare 2 selected cards
-  useEffect (() => {
+   // compare 2 selected cards
+   useEffect (() => {
     
     if(choiceUno && choiceDos){
       setDisabled(true)
@@ -84,34 +78,9 @@ function App() {
     // if choice1 is true 
     choiceUno ? setChoiceDos(card) : setChoiceUno(card)
   }
+
+export default function Game() {
   return (
-    
-    <div className="App">
-     
-      <h2>Culture Match</h2>
-      <button onClick={shuffleCards}>New Game</button>
-
-      <div className="card-grid">
-    {cards.map(card => (
-      // add card prop to access the card obj in single card comp
-      <SoloCard 
-      key = {card.id} 
-      card={card}
-      handleChoice = {handleChoice}
-      flipped = {card === choiceUno || card === choiceDos || card.matched}
-      disabled = {disabled}
-      />
-    ))}
-  </div>
-  <p id='try'>Trys: {trys} </p>
-  <Instructions />
-  
-  <Dictionary/>
-
-  
-    </div>
-    
-  );
+    <div>Game</div>
+  )
 }
-
-export default App;
